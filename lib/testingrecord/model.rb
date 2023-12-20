ff242d1870d853901f912cab261e6b4ed27ea493
Luke Hill <lukehill_uk@hotmail.com>
2023-12-20 09:09:10 +0000

    Add basic new class and extrapolate requires / tests

---
+++ lib/testingrecord/model.rb
@@ -0,0 +1,5 @@
+module Testingrecord
+  class Model
+
+  end
+end
---
+++ spec/testingrecord/model_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Testingrecord::Model do
+  it { is_expected.to be_a(Testingrecord::Model) }
+end
