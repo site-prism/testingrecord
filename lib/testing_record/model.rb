b235e7cbe3fb271220d604000a5fa796c4af66a1
Luke Hill <lukehill_uk@hotmail.com>
2023-12-20 14:53:56 +0000

    Create way of accessing the objects that need to be created

--- lib/testing_record/model.rb
+++ lib/testing_record/model.rb
@@ -1,15 +1,30 @@
 # frozen_string_literal: true
 
+require 'automation_helpers/extensions/string'
+
 module TestingRecord
   # The top level Model. Most of the behaviours specified here are fairly rudimentary ones that will then
   # include other behaviour(s), from the included modules
   class Model
     class << self
+      # Create a cache of the entities, named according to the classname
+      def create_accessible_collection!
+        ivar_name = "@#{name}s"
+        instance_variable_set(ivar_name, [])
+        define_singleton_method(:"#{name}s") { instance_variable_get(ivar_name) }
+      end
+
       # Set the type of model, this should be one of `:singular` or `:plural`
       #
       # @return [Symbol]
       def type(type)
         @type = type
+      end
+
+      private
+
+      def name
+        to_s.snake_case
       end
     end
   end
--- spec/testing_record/model_spec.rb
+++ spec/testing_record/model_spec.rb
@@ -1,27 +1,44 @@
 # frozen_string_literal: true
 
 RSpec.describe TestingRecord::Model do
-  context 'when classified as a singular model' do
-    let(:model) do
-      Class.new(TestingRecord::Model) do
-        type :singular
+  describe '.type' do
+    context 'when classified as a singular model' do
+      let(:model) do
+        Class.new(TestingRecord::Model) do
+          type :singular
+        end
+      end
+
+      it 'sets the singular model properties' do
+        expect(model.instance_variable_get(:@type)).to eq(:singular)
       end
     end
 
-    it 'sets the singular model properties' do
-      expect(model.instance_variable_get(:@type)).to eq(:singular)
+    context 'when classified as a plural model' do
+      let(:model) do
+        Class.new(TestingRecord::Model) do
+          type :plural
+        end
+      end
+
+      it 'sets the singular model properties' do
+        expect(model.instance_variable_get(:@type)).to eq(:plural)
+      end
     end
   end
 
-  context 'when classified as a plural model' do
-    let(:model) do
-      Class.new(TestingRecord::Model) do
-        type :plural
-      end
+  describe '.create_accessible_collection!' do
+    before do
+      stub_const('Foo', Class.new(described_class))
+      Foo.create_accessible_collection!
     end
 
-    it 'sets the singular model properties' do
-      expect(model.instance_variable_get(:@type)).to eq(:plural)
+    it 'generates a new reader class method for accessing the raw data' do
+      expect(Foo).to respond_to(:foos)
+    end
+
+    it 'auto-creates the cache as an empty array' do
+      expect(Foo.foos).to eq([])
     end
   end
 end
