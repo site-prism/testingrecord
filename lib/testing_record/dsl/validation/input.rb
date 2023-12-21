f57a89c8e46ea62866dceb88604c1f460761e1c2
Luke Hill <lukehill_uk@hotmail.com>
2023-12-21 13:59:02 +0000

    Add caching validations rules

--- lib/testing_record/dsl/validation/input.rb
+++ lib/testing_record/dsl/validation/input.rb
@@ -6,7 +6,14 @@ module TestingRecord
       # [TestingRecord::DSL::Validation::Input]
       # Validations for direct inputs into creating models
       module Input
-        # Check whether the type is valid
+        # Check whether the caching setting is valid
+        #
+        # @return [Boolean]
+        def caching_valid?(input)
+          caching_validations.include?(input)
+        end
+
+        # Check whether the type setting is valid
         #
         # @return [Boolean]
         def type_valid?(input)
@@ -15,6 +22,7 @@ module TestingRecord
 
         private
 
+        def caching_validations = %i[enabled disabled]
         def type_validations = %i[singular plural]
       end
     end
