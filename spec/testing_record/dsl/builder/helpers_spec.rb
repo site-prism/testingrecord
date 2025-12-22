3865371d643858e7820dee624fa61d9dff2a83b0
Luke Hill <lukehill_uk@hotmail.com>
2025-12-22 16:40:41 +0000

    Fix up test descriptions

--- spec/testing_record/dsl/builder/helpers_spec.rb
+++ spec/testing_record/dsl/builder/helpers_spec.rb
@@ -2,6 +2,7 @@
 
 RSpec.describe TestingRecord::DSL::Builder::Helpers do
   subject(:instance) { klazz.new }
+
   let(:klazz) do
     Class.new(TestingRecord::Model) do
       attribute :foo
@@ -10,34 +11,32 @@ RSpec.describe TestingRecord::DSL::Builder::Helpers do
   end
 
   describe '.add_helpers' do
-    context 'adds the presence helper to check if an attribute is present' do
-      it 'is true for defined strings' do
-        expect(klazz.create({ foo: 'bar' }).foo?).to be true
-      end
+    it 'adds the presence helper (true), for defined strings' do
+      expect(klazz.create({ foo: 'bar' }).foo?).to be true
+    end
 
-      it 'is true for defined symbols' do
-        expect(klazz.create({ foo: :bar }).foo?).to be true
-      end
+    it 'adds the presence helper (true), for defined symbols' do
+      expect(klazz.create({ foo: :bar }).foo?).to be true
+    end
 
-      it 'is true for defined non-empty arrays' do
-        expect(klazz.create({ foo: ['abc'] }).foo?).to be true
-      end
+    it 'adds the presence helper (true), for defined non-empty arrays' do
+      expect(klazz.create({ foo: ['abc'] }).foo?).to be true
+    end
 
-      it 'is false for defined nil values' do
-        expect(klazz.create({ foo: nil }).foo?).to be false
-      end
+    it 'adds the presence helper (false), for defined nil values' do
+      expect(klazz.create({ foo: nil }).foo?).to be false
+    end
 
-      it 'is false for defined empty arrays' do
-        expect(klazz.create({ foo: [] }).foo?).to be false
-      end
+    it 'adds the presence helper (false), for defined empty arrays' do
+      expect(klazz.create({ foo: [] }).foo?).to be false
+    end
 
-      it 'is false for defined empty hashes' do
-        expect(klazz.create({ foo: {} }).foo?).to be false
-      end
+    it 'adds the presence helper (false), for defined empty hashes' do
+      expect(klazz.create({ foo: {} }).foo?).to be false
+    end
 
-      it 'does not work undefined for undefined attributes' do
-        expect(klazz.create({ bar: 'value' })).not_to respond_to(:bar?)
-      end
+    it 'does not add a presence helper for undefined attributes' do
+      expect(klazz.create({ bar: 'value' })).not_to respond_to(:bar?)
     end
   end
 end
