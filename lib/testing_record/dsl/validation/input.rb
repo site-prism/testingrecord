1aa80222caf8bca4da97d12f48a416bd8c095b75
Luke Hill <lukehill_uk@hotmail.com>
2025-12-22 15:42:54 +0000

    Remove type validity checks

--- lib/testing_record/dsl/validation/input.rb
+++ lib/testing_record/dsl/validation/input.rb
@@ -13,17 +13,9 @@ module TestingRecord
           enabled_or_disabled.include?(input)
         end
 
-        # Check whether the type setting is valid
-        #
-        # @return [Boolean]
-        def type_valid?(input)
-          singular_or_plural.include?(input)
-        end
-
         private
 
         def enabled_or_disabled = %i[enabled disabled]
-        def singular_or_plural = %i[singular plural]
       end
     end
   end
