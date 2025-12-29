f43a8f8819beda379792612a739b19a119d80778
Luke Hill <lukehill_uk@hotmail.com>
2025-12-29 12:19:01 +0000

    Add in all scaffolding logic for logger testing

--- lib/testing_record.rb
+++ lib/testing_record.rb
@@ -10,25 +10,27 @@ module TestingRecord
   # Generic TestingRecord error. Will be extended in the future
   class Error < StandardError; end
 
-  # The Testing Record logger object - This is called automatically in several
-  # locations and will log messages according to the normal Ruby protocol
-  #
-  # This logger object can also be used to manually log messages
-  #
-  # To Manually log a message
-  #   TestingRecord.logger.info('Information')
-  #   TestingRecord.logger.debug('Input debug message')
-  #
-  # By default the logger will output all messages to $stdout, but can be
-  # altered to log to a file or another IO location by calling `.log_path=`
-  def logger
-    @logger ||= Logger.create
-  end
+  class << self
+    # The Testing Record logger object - This is called automatically in several
+    # locations and will log messages according to the normal Ruby protocol
+    #
+    # This logger object can also be used to manually log messages
+    #
+    # To Manually log a message
+    #   TestingRecord.logger.info('Information')
+    #   TestingRecord.logger.debug('Input debug message')
+    #
+    # By default the logger will output all messages to $stdout, but can be
+    # altered to log to a file or another IO location by calling `.log_path=`
+    def logger
+      @logger ||= Logger.create
+    end
 
-  # This writer method allows you to configure where you want the testingrecord logs to be sent to (Default is $stdout)
-  #
-  #   Example: TestingRecord.log_path = 'testingrecord.log' would save all log messages to `./testingrecord.log`
-  def log_path=(logdev)
-    logger.reopen(logdev)
+    # This writer method allows you to configure where you want the testingrecord logs to be sent to (Default is $stdout)
+    #
+    #   Example: TestingRecord.log_path = 'testingrecord.log' would save all log messages to `./testingrecord.log`
+    def log_path=(logdev)
+      logger.reopen(logdev)
+    end
   end
 end
---
+++ spec/testing_record_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+describe TestingRecord do
+  # Stop the $stdout process leaking cross-tests
+  before { wipe_logger! }
+
+  describe '.logger' do
+    it 'logs messages at every level by default' do
+      log_messages = capture_stdout do
+        described_class.logger.debug('DEBUG')
+        described_class.logger.unknown('UNKNOWN')
+      end
+
+      expect(lines(log_messages)).to eq(2)
+    end
+  end
+
+  describe '.log_path=' do
+    context 'when set to a file' do
+      let(:filename) { 'sample.log' }
+      let(:file_content) { File.read(filename) }
+
+      before { described_class.log_path = filename }
+
+      after { FileUtils.rm_f(filename)  }
+
+      it 'sends the log messages to the file-path provided' do
+        described_class.logger.unknown('This is sent to the file')
+
+        expect(file_content).to end_with("This is sent to the file\n")
+      end
+    end
+
+    context 'when set to $stderr' do
+      it 'sends the log messages to $stderr' do
+        expect do
+          described_class.log_path = $stderr
+          described_class.logger.unknown('This is sent to $stderr')
+        end.to output(/This is sent to \$stderr/).to_stderr
+      end
+    end
+  end
+end
