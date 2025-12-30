9b904805d4bc7eb7c99b14c6f78c0767cd5be8a3
Luke Hill <lukehill_uk@hotmail.com>
2025-12-30 13:18:58 +0000

    Implement new basic filter logic

--- lib/testing_record/dsl/builder.rb
+++ lib/testing_record/dsl/builder.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+require_relative 'builder/filters'
 require_relative 'builder/helpers'
 require_relative 'builder/settings'
--- lib/testing_record/model.rb
+++ lib/testing_record/model.rb
@@ -7,6 +7,7 @@ module TestingRecord
   # The top level Model. Most of the behaviours specified here are fairly rudimentary ones that will then
   # include other behaviour(s), from the included modules
   class Model
+    extend DSL::Builder::Filters
     extend DSL::Builder::Helpers
     extend DSL::Builder::Settings
 
