9ef7c2244e8b1cc7ef7f700e5af93bd5e074cbdc
Luke Hill <lukehill_uk@hotmail.com>
2026-05-25 12:33:14 +0100

    Add in unit tests for validator

--- spec/testing_record/dsl/validation/input_spec.rb
+++ spec/testing_record/dsl/validation/input_spec.rb
@@ -8,16 +8,30 @@ RSpec.describe TestingRecord::DSL::Validation::Input do
   end
 
   describe '.caching_valid?' do
-    it 'is `true` when the type is enabled' do
+    it 'is `true` when the type is :enabled' do
       expect(klazz.caching_valid?(:enabled)).to be true
     end
 
-    it 'is `true` when the type is disabled' do
+    it 'is `true` when the type is :disabled' do
       expect(klazz.caching_valid?(:disabled)).to be true
     end
 
     it 'is `false` for all other types' do
       expect(klazz.caching_valid?(:foo)).to be false
+    end
+  end
+
+  describe '.filter_logic_valid??' do
+    it 'is `true` when the type is :and' do
+      expect(klazz.filter_logic_valid?(:and)).to be true
+    end
+
+    it 'is `true` when the type is :or' do
+      expect(klazz.filter_logic_valid?(:or)).to be true
+    end
+
+    it 'is `false` for all other types' do
+      expect(klazz.filter_logic_valid?(:foo)).to be false
     end
   end
 end
